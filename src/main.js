e30f3d78853e293394d230483903cb2c2d531ce4
sihle <gubug777@gmail.com>
2022-04-21 09:51:56 +0800

    add vue-axios pkg

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '@/assets/scss/all.scss';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(router);
+app.use(VueAxios, axios)
+app.mount('#app');
