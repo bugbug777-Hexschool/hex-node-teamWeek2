2f528dbdb3ab66a00deaa96fd82af635ab9dd97c
sihle <gubug777@gmail.com>
2022-04-21 10:30:50 +0800

    update axios import order

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
-import axios from 'axios'
-import VueAxios from 'vue-axios'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '@/assets/scss/all.scss';
 
 const app = createApp(App);
 app.use(router);
-app.use(VueAxios, axios)
+app.use(VueAxios, axios);
 app.mount('#app');
